0aaa098b91440d835971c52397b2f4fc5b89a15a
Akkrapol Kanpong <53117897+touchmylike2019@users.noreply.github.com>
2020-01-07 04:00:49 +0700

    update env

--- wp-config.php
+++ wp-config.php
@@ -1,35 +1,19 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
+require __DIR__.'/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'touchmylike' );
+define( 'DB_NAME', getenv('DB_NAME'));
 
-/** MySQL database username */
-define( 'DB_USER', 'root' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', '' );
-
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+    /** MySQL database username */
+define( 'DB_USER', getenv('DB_USER'));
+    
+    /** MySQL database password */
+define( 'DB_PASSWORD', getenv('DB_PASSWORD'));
+    
+    /** MySQL hostname */
+define( 'DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
